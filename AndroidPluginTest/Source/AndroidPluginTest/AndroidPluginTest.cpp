ace69833b134fed3009f023811a3b374a74fd5c7
Tim Graupmann <tim@tagenigma.com>
2016-04-24 00:35:56 -0700

    Added a unit test to verify the Android input callback is being invoked in a UE4 plugin

--- AndroidPluginTest/Source/AndroidPluginTest/AndroidPluginTest.cpp
+++ AndroidPluginTest/Source/AndroidPluginTest/AndroidPluginTest.cpp
@@ -2,4 +2,44 @@
 
 #include "AndroidPluginTest.h"
 
+// this test is Android specific
+#if PLATFORM_ANDROID
+
+// Get access to Android logging
+#include <android/log.h>
+
+// The Android Input callback is defined in the launcher code
+#include "../../../Launch/Public/Android/LaunchAndroid.h"
+
+// Redefine a tag for logging
+#ifdef LOG_TAG
+#undef LOG_TAG
+#endif
+#define LOG_TAG "AndroidPluginTest"
+
+#endif
+
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AndroidPluginTest, "AndroidPluginTest" );
+
+// this test is Android specific
+#if PLATFORM_ANDROID
+
+// Use a global variable to cause the constructor to invoke
+AndroidPluginTestSetupCallbackAndroidInput GSetupCallbackAndroidInput;
+
+// define the callback function that will get the android input events
+int32_t AndroidPluginTestHandleRegisterCallbackAndroidInput(struct android_app* app, AInputEvent* event)
+{
+	// check the adb logcat
+	__android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, "*** This indicates a successful test. The callback was invoked! ***");
+
+	return 0;
+}
+
+// Use the constructor to register the input callback using the global var: `GSetupCallbackAndroidInput`
+AndroidPluginTestSetupCallbackAndroidInput::AndroidPluginTestSetupCallbackAndroidInput()
+{
+	RegisterCallbackAndroidInput(AndroidPluginTestHandleRegisterCallbackAndroidInput);
+}
+
+#endif
--- AndroidPluginTest/Source/AndroidPluginTest/AndroidPluginTest.h
+++ AndroidPluginTest/Source/AndroidPluginTest/AndroidPluginTest.h
@@ -4,3 +4,28 @@
 
 #include "Engine.h"
 
+/**
+ * This is a unit test to verify that the Android input callback is properly
+ * invoked on Android.
+ * A successful test will display a message in the Android logcat.
+ */
+#if PLATFORM_ANDROID
+
+/*
+ * In this case `Unit Test` is meant as a
+ * test created by the developer to make sure
+ * a particular feature works.
+ * This test uses the class constructor
+ * to register the callback by
+ * constructing a global variable
+ * which happens before any Android input
+ * events.
+ */
+class AndroidPluginTestSetupCallbackAndroidInput
+{
+public:
+	// The constructor will register the Android Input callback
+	AndroidPluginTestSetupCallbackAndroidInput();
+};
+
+#endif
