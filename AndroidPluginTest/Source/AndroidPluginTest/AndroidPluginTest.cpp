49d9599e69629960a48a418c7e2fd5c8e5062521
Tim Graupmann <tim@tagenigma.com>
2016-04-23 21:38:53 -0700

    Created UE4 plugin to test remapped input

---
+++ AndroidPluginTest/Source/AndroidPluginTest/AndroidPluginTest.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AndroidPluginTest.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AndroidPluginTest, "AndroidPluginTest" );
